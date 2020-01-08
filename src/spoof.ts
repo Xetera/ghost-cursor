7c0019ca9eab39ee71abda5a8a0f74d8810bde01
Xetera <mythologyali@gmail.com>
2020-01-07 22:38:21 -0800

    click() now takes an optional selector

--- src/spoof.ts
+++ src/spoof.ts
@@ -1,4 +1,4 @@
-import type { Page } from "puppeteer";
+import { Page } from "puppeteer";
 import {
   Vector,
   bezierCurve,
@@ -95,12 +95,23 @@ const shouldOvershoot = (a: Vector, b: Vector) =>
   magnitude(direction(a, b)) > overshootThreshold;
 
 export const createCursor = (page: Page, start: Vector = origin) => {
+  // this is kind of arbitrary, not a big fan but it seems to work
+  const overshootSpread = 10;
   const overshootRadius = 120;
-  // this is kind of arbitrary, not a big fan
   let previous: Vector = start;
-  return {
-    click: () => page.mouse.down().then(() => page.mouse.up()),
-    move: async (selector: string) => {
+  const tracePath = async (vectors: Iterable<Vector>) => {
+    for (const { x, y } of vectors) {
+      await page.mouse.move(x, y);
+    }
+  };
+  const actions = {
+    async click(selector?: string) {
+      if (selector) {
+        await actions.move(selector);
+      }
+      return page.mouse.down().then(() => page.mouse.up());
+    },
+    async move(selector: string) {
       const elem = await page.$(selector);
       if (!elem) {
         throw new Error(
@@ -110,7 +121,7 @@ export const createCursor = (page: Page, start: Vector = origin) => {
       const box = await elem.boundingBox();
       if (!box) {
         throw new Error(
-          "Could not find dimensions of the element you're clicking on, this might be a bug?"
+          "Could not find the dimensions of the element you're clicking on, this might be a bug?"
         );
       }
       const { height, width } = box;
@@ -120,115 +131,19 @@ export const createCursor = (page: Page, start: Vector = origin) => {
       const to = overshooting
         ? overshoot(destination, overshootRadius)
         : destination;
-      for (const { x, y } of path(previous, to)) {
-        await page.mouse.move(x, y);
-      }
+      await tracePath(path(previous, to));
+
       if (overshooting) {
-        for (const { x, y } of path(
+        const correction = path(
           to,
           { ...dimensions, ...destination },
-          10
-        )) {
-          await page.mouse.move(x, y);
-        }
+          overshootSpread
+        );
+
+        await tracePath(correction);
       }
       previous = destination;
     }
   };
+  return actions;
 };
-
-// (async () => {
-//   console.log("Launching browser.");
-//   const options = {
-//     height: 600,
-//     width: 1200
-//   };
-//   const browser = await puppeteer.launch({
-//     headless: false,
-//     args: ["--start-maximized"]
-//   });
-
-//   const page = await browser.newPage();
-//   await page.setViewport({ width: 1920, height: 1080 });
-//   await installMouseHelper(page);
-//   const url = "https://nike.com";
-//   const loginButton = "#AccountNavigationContainer";
-//   await page.goto(url);
-//   // await page.evaluate(mountForm, accCreateForm);
-//   console.log("waiting for login");
-//   await page.waitForSelector(loginButton);
-//   await page.waitFor(500);
-//   const pointer = createCursor(page, { x: 0, y: 700 });
-//   console.log("found button");
-//   await pointer.move(loginButton);
-//   await pointer.click();
-//   await page.waitForSelector(".loginJoinLink a");
-//   await page.waitFor(500);
-//   await pointer.move(".loginJoinLink a");
-//   await pointer.click();
-//   for (const step of steps) {
-//     await pointer.move(step.selector);
-//     await pointer.click();
-//     if (step.value) {
-//       page.evaluate(
-//         // @ts-ignore
-//         step => (document.querySelector(step.selector).value = step.value),
-//         step
-//       );
-//     }
-//   }
-// })();
-
-// const height = 850;
-// const width = 1500;
-// const s = p5 => {
-//   p5.setup = () => {
-//     p5.resizeCanvas(width, height);
-//   };
-
-//   const boxWidth = 50;
-//   p5.draw = () => {
-//     p5.noLoop();
-//     p5.background(51);
-//     const s = p5.color("turquoise");
-//     p5.noStroke();
-//     p5.fill(s);
-//     p5.stroke(255);
-//     p5.noFill();
-//     p5.beginShape();
-//     const start = { x: 10, y: 10 };
-//     const end = { x: 1200, y: 700 };
-//     const stop = overshoot(end, 100);
-//     console.log("stop", stop);
-//     const [bez, anchors] = bezierCurve(start, stop);
-//     console.log(bez.length());
-//     const steps = fitts(start, end, 100);
-//     console.log(steps);
-//     console.log(Math.log2(steps));
-//     const points = bez.getLUT();
-//     for (const point of points) {
-//       p5.vertex(point.x, point.y);
-//     }
-//     p5.endShape();
-//     p5.beginShape();
-//     const [bez2, anchors2] = bezierCurve(stop, end);
-//     console.log(bez.length());
-//     const steps2 = fitts(start, end, 100);
-//     console.log(steps);
-//     console.log(Math.log2(steps));
-//     const points2 = bez2.getLUT();
-//     for (const point of points2) {
-//       p5.vertex(point.x, point.y);
-//     }
-//     for (const anchor of anchors) {
-//       p5.circle(anchor.x, anchor.y, 10);
-//     }
-//     // for (const point of points2) {
-//     //   p5.vertex(point.x, point.y);
-//     // }
-//     p5.endShape();
-//   };
-// };
-
-// const p5 = new p(s);
-//
