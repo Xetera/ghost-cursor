1a9862f6305b7c6301e474e2c5d100e42d7f788e
Niek van der Maas <mail@niekvandermaas.nl>
2020-11-04 13:29:48 +0100

    Do not lint mouse-helper

--- src/mouse-helper.ts
+++ src/mouse-helper.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
 // This injects a box into the page that moves with the mouse;
 // Useful for debugging
-async function installMouseHelper(page) : void {
+async function installMouseHelper(page) {
   await page.evaluateOnNewDocument(() => {
     // Install mouse helper only for top-level frame.
     if (window !== window.parent) return;
